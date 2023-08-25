fb4177daaf5b4fe310875d3fb1c0861165dfe9af
yopa <90709631+yopaaa@users.noreply.github.com>
2023-08-25 18:02:59 +0700

    Update ASCIIArt.js

--- js/ASCIIArt.js
+++ js/ASCIIArt.js
@@ -1,15 +1,15 @@
-import figlet from "figlet"
-import chalk from "chalk"
-import moment from "moment"
-import { internalIpV4 } from "internal-ip"
+import figlet from 'figlet'
+import chalk from 'chalk'
+import moment from 'moment'
+import { internalIpV4 } from 'internal-ip'
 
 const localIp = await internalIpV4()
 const port = process.env.MAIN_PORT
 const LogASCIIText = (text) => {
   const config = {
-    font: "Big Money-ne",
-    horizontalLayout: "default",
-    verticalLayout: "default",
+    font: 'Big Money-ne',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
     width: 80,
     whitespaceBreak: true
   }
@@ -20,10 +20,10 @@ const LogASCIIText = (text) => {
   figlet.text(text, config, (err, data) => {
     if (err) console.log(err)
     console.log(chalk.green(data))
-    console.log(`    Running on :  ${moment().format("llll")}
+    console.log(`    Running on :  ${moment().format('llll')}
     You can now connect in on end point.
 
-    Running         : ${process.env.NODE_ENV || "no set"}
+    Running         : ${process.env.NODE_ENV || 'no set'}
     Local           : http://localhost:${port}
     On Your Network : http://${localIp}:${port}
     `)
