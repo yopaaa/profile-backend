ba94392925b318cf9aceaac2c0ad933de89ef311
yopa <90709631+yopaaa@users.noreply.github.com>
2023-08-26 18:03:26 +0700

    Update check_data_dir.js

--- js/check_data_dir.js
+++ js/check_data_dir.js
@@ -1,11 +1,11 @@
-import fs from "fs"
+import fs from 'fs'
 
 // check if directory exists
-if (!fs.existsSync("./data")) {
-  fs.mkdir("./data", () => 0)
+if (!fs.existsSync('./data')) {
+  fs.mkdir('./data', () => 0)
 }
-if (!fs.existsSync("./data/log.txt")) {
-  fs.writeFile("./data/log.txt", "", (err) => {
+if (!fs.existsSync('./data/log.txt')) {
+  fs.writeFile('./data/log.txt', '', (err) => {
     if (err) throw err
   })
 }
