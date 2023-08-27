2ee14e72dcf5429ff8a99b7b832bff3df7a66d22
yopa <90709631+yopaaa@users.noreply.github.com>
2023-08-28 01:03:49 +0700

    Update log.js

--- js/log.js
+++ js/log.js
@@ -1,38 +1,32 @@
-import fs from "fs"
-import chalk from "chalk"
-import "dotenv/config"
-import moment from "moment"
+import fs from 'fs'
+import chalk from 'chalk'
+import 'dotenv/config'
+import moment from 'moment'
 
 const logFilePath = process.env.LOG_FILE_PATH
 
-function log(text, code = 200, method = "NULL") {
+function log(text, code = 200, method = 'NULL') {
   const time = moment().format()
   let color, bgColor
-  if (code >= 400 && code < 500 || method == 'DELETE') {
-    color = "yellow"
-    bgColor = "bgYellow"
+  if ((code >= 400 && code < 500) || method === 'DELETE') {
+    color = 'yellow'
+    bgColor = 'bgYellow'
   } else if (code >= 500 && code < 600) {
-    color = "red"
-    bgColor = "bgRed"
+    color = 'red'
+    bgColor = 'bgRed'
   } else {
-    color = "green"
-    bgColor = "bgGreen"
+    color = 'green'
+    bgColor = 'bgGreen'
   }
   const background = (text) => chalk[bgColor](chalk.black(text))
 
-  const msglog = `${background(code)} | ${time} | ${background(
-    method
-  )} | ${chalk[color](text)}`
+  const msglog = `${background(code)} | ${time} | ${background(method)} | ${chalk[color](text)}`
   const msgSave = `${code} | ${time} | ${method} | ${text} \n`
 
   if (logFilePath) {
-    fs.appendFile(
-      `${logFilePath}/${moment().format("LL").replaceAll(" ", "-")}.txt`,
-      msgSave,
-      (err) => {
-        if (err != null) console.log(err)
-      }
-    )
+    fs.appendFile(`${logFilePath}/${moment().format('LL').replaceAll(' ', '-')}.txt`, msgSave, (err) => {
+      if (err != null) console.log(err)
+    })
   }
 
   console.log(msglog)
