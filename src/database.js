d2e555da2eb04ba6a8dd140d446328a0e0bcae0b
yopa <90709631+yopaaa@users.noreply.github.com>
2023-09-13 20:21:26 +0700

    Update database.js

--- src/database.js
+++ src/database.js
@@ -149,6 +149,156 @@ const visitorsSchema = {
   }
 }
 
+const anonymousMsgSchema = {
+  _id: String,
+  msg: String,
+  user: String,
+  ua: {
+    type: String,
+    required: true
+  },
+  browser: {
+    name: {
+      type: String,
+      default: null
+    },
+    version: {
+      type: String,
+      default: null
+    },
+    major: {
+      type: String,
+      default: null
+    }
+  },
+  engine: {
+    name: {
+      type: String,
+      default: null
+    },
+    version: {
+      type: String,
+      default: null
+    }
+  },
+  device: {
+    name: {
+      type: String,
+      default: null
+    },
+    version: {
+      type: String,
+      default: null
+    }
+  },
+  os: {
+    name: {
+      type: String,
+      default: null
+    },
+    version: {
+      type: String,
+      default: null
+    }
+  },
+  cpu: {
+    architecture: {
+      type: String,
+      default: null
+    }
+  },
+  visitor: {
+    ip: {
+      type: String,
+      default: null
+    },
+    network: {
+      type: String,
+      default: null
+    },
+    version: {
+      type: String,
+      default: null
+    },
+    city: {
+      type: String,
+      default: null
+    },
+    region: {
+      type: String,
+      default: null
+    },
+    region_code: {
+      type: String,
+      default: null
+    },
+    country: {
+      type: String,
+      default: null
+    },
+    country_name: {
+      type: String,
+      default: null
+    },
+    country_code: {
+      type: String,
+      default: null
+    },
+    country_code_iso3: {
+      type: String,
+      default: null
+    },
+    country_capital: {
+      type: String,
+      default: null
+    },
+    country_tld: {
+      type: String,
+      default: null
+    },
+    continent_code: {
+      type: String,
+      default: null
+    },
+    in_eu: {
+      type: Boolean,
+      default: null
+    },
+    postal: {
+      type: String,
+      required: false
+    },
+    timezone: {
+      type: String,
+      default: null
+    },
+    utc_offset: {
+      type: String,
+      default: null
+    },
+    country_calling_code: {
+      type: String,
+      default: null
+    },
+    currency: {
+      type: String,
+      default: null
+    },
+    currency_name: {
+      type: String,
+      default: null
+    },
+    languages: {
+      type: String,
+      default: null
+    },
+    org: {
+      type: String,
+      default: null
+    }
+  }
+}
+
 const dataSchema = {
   _id: String,
   name: String,
@@ -209,6 +359,7 @@ const dataSchema = {
 }
 
 const usersData = new Database('profile-data', dataSchema)
+const anonymousMsgData = new Database('anonymousMsgCollections', anonymousMsgSchema)
 const users = {
   yopaaa: {
     visitors: new Database('visitors-yopaaa', visitorsSchema)
@@ -218,4 +369,4 @@ const users = {
   }
 }
 
-export { visitorsSchema, users, usersData }
+export { visitorsSchema, users, usersData, anonymousMsgData }
